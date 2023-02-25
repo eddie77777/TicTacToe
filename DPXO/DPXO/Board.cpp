36ece6427245e7711f419bc5f97375c12b7498d3
eddie77777 <eddiedragomir17@gmail.com>
2023-02-25 14:39:48 +0200

    Board header class

---
+++ DPXO/DPXO/Board.cpp
@@ -0,0 +1,11 @@
+#include "Board.h"
+
+bool Board::CheckIfAddOnPos()
+{
+	return false;
+}
+
+bool Board::WinOrDraw()
+{
+	return false;
+}
---
+++ DPXO/DPXO/Board.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <array>
+#include <set>
+
+class Board
+{
+public:
+	static const size_t kWidth = 3;
+	static const size_t kHeight = 3;
+	static const size_t kSize = kWidth * kHeight;
+
+public:
+	Board() = default;
+
+public:
+	bool CheckIfAddOnPos();
+	bool WinOrDraw();
+
+private:
+	std::array<std::array<char, kWidth>, kHeight> m_board;
+	std::set<std::pair<int, int>> m_emptyPos;
+	enum class States {
+		NONE,
+		WIN,
+		TIE
+	};
+};
+
